7108e312e97e1db0cdbac5cb3947124ff06a3403
Sylvain Willy <willy.sylvain@gmail.com>
2019-10-31 00:51:14 +0100

    client.py cleanup

--- steps/client.py
+++ steps/client.py
@@ -3,12 +3,6 @@ from behave import *
 from peerster import Client
 
 
-@given('a node "{name}"')
-def step_impl(context, name):
-    context.nodes[name] = {'logs': ""}
-
-
 @when('a client sends "{name}" a message "{message}"')
 def step_impl(context, name, message):
-    output = Client.send_to(name, message)
-    print(output)
+    Client.send_to(name, message)
