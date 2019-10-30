f60875459c70f148f726a4e47167c1285bc08a5c
Sylvain Willy <willy.sylvain@gmail.com>
2019-10-31 00:42:30 +0100

    Add environment cleanup

--- environment.py
+++ environment.py
@@ -2,9 +2,16 @@ import subprocess
 
 
 def before_feature(context, feature):
+    subprocess.call(["killall", "Peerster"])
     assert subprocess.call(["go", "build"]) is 0
     assert subprocess.call(["go", "build"], cwd='./client') is 0
 
 
 def before_scenario(context, scenario):
     context.nodes = {}
+
+
+def after_scenario(context, scenario):
+    for node in context.nodes.values():
+        node.process.terminate()
+    subprocess.call(["killall", "Peerster"])
