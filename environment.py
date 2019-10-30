59ae67805953d9665d699c48ed863ba9d6944944
Sylvain Willy <willy.sylvain@gmail.com>
2019-10-30 23:41:56 +0100

    Trigger go build before each feature run

--- environment.py
+++ environment.py
@@ -1,2 +1,10 @@
+import subprocess
+
+
+def before_feature(context, feature):
+    assert subprocess.call(["go", "build"]) is 0
+    assert subprocess.call(["go", "build"], cwd='./client') is 0
+
+
 def before_scenario(context, scenario):
     context.nodes = {}
