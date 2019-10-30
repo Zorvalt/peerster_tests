5c2b3e860d211fed7451e7d77c84e12c9b93bf00
Sylvain Willy <willy.sylvain@gmail.com>
2019-10-31 00:53:53 +0100

    Add not simple node spawning step

--- steps/gossiper.py
+++ steps/gossiper.py
@@ -1,5 +1,14 @@
 from behave import *
 
+from peerster import Gossiper
+
+
+@given('a node "{name}"')
+def step_impl(context, name):
+    if name in context.nodes.keys():
+        raise EnvironmentError("The same node has been asked twice")
+    context.nodes[name] = Gossiper(name)
+
 
 @then('the node "{name}" should have logged the received message "{message}" from a client')
 def step_impl(context, name, message):
