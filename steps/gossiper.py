36b118328743030fd590b443d91a543131371a3c
Marc Gay-Balmaz <m.gaybalmaz@gmail.com>
2019-10-31 17:35:14 +0100

    Fix step order to avoid ambiguity

--- steps/gossiper.py
+++ steps/gossiper.py
@@ -3,18 +3,18 @@ from behave import *
 from peerster import Gossiper
 
 
+@given('a node "{name}" knowing "{neighbors}"')
+def step_impl(context, name, neighbors):
+    if name in context.nodes.keys():
+        raise EnvironmentError("The same node has been asked twice")
+    context.nodes[name] = Gossiper(name, peers=neighbors)
+
+
 @given('a node "{name}"')
 def step_impl(context, name):
     if name in context.nodes.keys():
         raise EnvironmentError("The same node has been asked twice")
     context.nodes[name] = Gossiper(name)
-
-
-@given('a node "{name} knowing {neighbors}"')
-def step_impl(context, name, neighbors):
-    if name in context.nodes.keys():
-        raise EnvironmentError("The same node has been asked twice")
-    context.nodes[name] = Gossiper(name, peers=neighbors)
 
 
 @then('the node "{name}" should have logged the received message "{message}" from a client')
