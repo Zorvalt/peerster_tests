03be1600c77e216e7f4ce0f0a355463635426484
Sylvain Willy <willy.sylvain@gmail.com>
2019-10-31 18:45:09 +0100

    Fix Gossiper's peers init

--- peerster.py
+++ peerster.py
@@ -26,7 +26,7 @@ class Client:
 
     @staticmethod
     def send_to_port(ui_port: int, message: str) -> str:
-        command = ['./client/client', '-UIPort=' + str(ui_port), '-msg=' + message]
+        command = ['./client/client', '-UIPort=' + str(ui_port), '-msg="' + message+'"']
         return subprocess.check_output(command).decode()
 
 
@@ -35,23 +35,25 @@ class Gossiper:
         offset = gossiper_name_to_port_offset(name)
         gossip_port = BASE_GOSSIP_PORT + offset
         ui_port = BASE_UI_PORT + offset
-        peers_str = ""
-        for peer in peers.split(","):
-            peers_str += "127.0.0.1:" + str(BASE_UI_PORT + gossiper_name_to_port_offset(peer)) + ","
 
-        output_file = tempfile.NamedTemporaryFile('w+')
-        self.output_buffer = []
         command = [
             './Peerster',
             '-name=' + name,
             '-gossipAddr=127.0.0.1:' + str(gossip_port),
             '-UIPort=' + str(ui_port),
             '-GUIPort=' + str(ui_port),
-            '-peers=' + peers_str[:-1],
         ]
         if simple:
             command.append('-simple')
-        self.process = subprocess.Popen(command, stdout=output_file)
+
+        if len(peers) > 0:
+            peers_str = ""
+            for peer in peers.split(","):
+                peers_str += "127.0.0.1:" + str(BASE_GOSSIP_PORT + gossiper_name_to_port_offset(peer)) + ","
+            command.append('-peers=' + peers_str[:-1])
+
+        output_file = tempfile.NamedTemporaryFile('w+')
+        self.process = subprocess.Popen(command, stdout=output_file, stderr=output_file)
         self.output_file = open(output_file.name, 'r')
         time.sleep(0.2)
 
