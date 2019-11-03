e4472d4954e87b38f34c6c7b3801f8908a150bae
Marc Gay-Balmaz <m.gaybalmaz@gmail.com>
2019-11-03 22:56:39 +0100

    Add steps to send private message

--- steps/client.py
+++ steps/client.py
@@ -3,6 +3,17 @@ from behave import *
 from peerster_objects.client import Client
 
 
+@when('a client sends "{sender}" a private message "{message}" to "{destination}"')
+def step_impl(context, sender, message, destination):
+    """
+    :param destination:
+    :param message:
+    :param sender:
+    :type context: behave.runner.Context
+    """
+    Client.send_private_to(sender, message, destination)
+
+
 @when('a client sends "{name}" a message "{message}"')
 def step_impl(context, name, message):
     Client.send_to(name, message)
